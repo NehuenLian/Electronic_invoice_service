d79a42f36a22cf6407ba1994288982628fa73b98
Nehuen Lian Bova <nehuenn.lian@gmail.com>
2025-06-13 19:47:37 -0300

    added an API endpoint and Pydantic model.

---
+++ service/api/app.py
@@ -0,0 +1,18 @@
+from service.controllers.request_invoice_controller import request_invoice_controller
+from service.utils.convert_model_to_dict import convert_pydantic_model_to_dict
+from fastapi import FastAPI
+from service.api.models.invoice import RootModel
+from service.utils.logger import logger
+
+
+app = FastAPI()
+
+@app.post("/invoice")
+def generate_invoice(sale_data: RootModel) -> dict:
+    
+    logger.info("Invoice generation request received. Parsing input data and calling controller.")
+
+    parsed_data = convert_pydantic_model_to_dict(sale_data)
+    CAE_response = request_invoice_controller(parsed_data)
+
+    return CAE_response
---
+++ service/api/models/invoice.py
@@ -0,0 +1,46 @@
+from pydantic import BaseModel
+
+class AlicIva(BaseModel):
+    Id: int
+    BaseImp: float
+    Importe: float
+
+class Iva(BaseModel):
+    AlicIva: AlicIva
+
+class FECAEDetRequest(BaseModel):
+    Concepto: int
+    DocTipo: int
+    DocNro: int
+    CbteDesde: int
+    CbteHasta: int
+    CbteFch: str
+    ImpTotal: float
+    ImpTotConc: float
+    ImpNeto: float
+    ImpOpEx: float
+    ImpTrib: float
+    ImpIVA: float
+    MonId: str
+    MonCotiz: float
+    CondicionIVAReceptorId: int
+    Iva: Iva
+
+class FeDetReq(BaseModel):
+    FECAEDetRequest: FECAEDetRequest
+
+class FeCabReq(BaseModel):
+    CantReg: int
+    PtoVta: int
+    CbteTipo: int
+
+class FeCAEReq(BaseModel):
+    FeCabReq: FeCabReq
+    FeDetReq: FeDetReq
+
+class Auth(BaseModel):
+    Cuit: int
+
+class RootModel(BaseModel):
+    Auth: Auth
+    FeCAEReq: FeCAEReq
