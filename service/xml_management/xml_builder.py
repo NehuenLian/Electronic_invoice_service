724269360f92b4178884fcaa249b99859747afb5
Nehuen Lian Bova <nehuenn.lian@gmail.com>
2025-06-19 13:32:59 -0300

    add a function that verifies if expirationTime is reached

--- service/xml_management/xml_builder.py
+++ service/xml_management/xml_builder.py
@@ -5,6 +5,9 @@ from lxml import etree
 from service.time.time_management import generate_timestamp
 from service.utils.logger import logger
 
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+
 
 def build_login_ticket_request():
 
@@ -42,6 +45,7 @@ def parse_and_save_loginticketresponse(login_ticket_response: str):
 
     logger.debug("loginTicketResponse.xml was successfully built.")
 
+    logger.debug("Renovating... loginTicketResponse.xml")
     save_xml(root, "loginTicketResponse.xml")
 
     logger.debug("loginTicketResponse.xml saved in 'xml_management/' folder.")
@@ -63,6 +67,28 @@ def extract_token_and_sign_from_loginticketresponse(xml_name: str) -> tuple[str,
     logger.debug("Token and Sign obtained successfully.")
 
     return token, sign
+
+def is_expiration_time_reached():
+    
+    logger.debug("Verifying if <expirationTime> expired.")
+
+    _, actual_hour_epoch, _ = generate_timestamp() # Obtiene el epoch actual, ignorando los otros valores ya que es una funcion auxiliar
+    baires_tz = ZoneInfo("America/Argentina/Buenos_Aires") # Saca la zona horaria 
+
+    path = f"service/xml_management/loginTicketRequest" # Path del xml del que se va a extraer el string con la hora para comparar con la actual
+    tree = etree.parse(path)
+    root = tree.getroot()
+    expiration_time_label = root.find(".//expirationTime") # Etiqueta a buscar
+    expiration_time_str = expiration_time_label.text # Se convierte a string la etiqueta obtenida
+
+    expiration_dt_naive = datetime.strptime(expiration_time_str, "%Y-%m-%dT%H:%M:%S") # Se convierte el string de la hora a un objeto datetime
+    expiration_dt = expiration_dt_naive.replace(tzinfo=baires_tz) # Se le agrega la zona horaria al objeto datetime
+    expiration_epoch = int(expiration_dt.timestamp()) # Se convierte el objeto datetime a epoch/entero
+
+    if actual_hour_epoch >= expiration_epoch:
+        return True
+    else:
+        return False
 
 def save_xml(root, xml_name):
     
