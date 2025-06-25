d926cb07bd17c542d76472462fa711f32157d2ad
Nehuen Lian Bova <nehuenn.lian@gmail.com>
2025-06-25 18:54:51 -0300

    add: Added custom exceptions AlreadyValidAT and loginTicketResponseNotFound.

--- service/exceptions.py
+++ service/exceptions.py
@@ -1,1 +1,7 @@
-# Custom exceptions here
+class AlreadyValidAT(Exception):
+    def __init__(self, original_exception):
+        super().__init__(str(original_exception))
+        self.original_exception = original_exception
+
+class loginTicketResponseNotFound(Exception):
+    pass
