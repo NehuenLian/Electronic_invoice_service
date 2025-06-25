5e1017b6422b1decb0a198200003ef446f5dd4d2
Nehuen Lian Bova <nehuenn.lian@gmail.com>
2025-06-25 18:53:20 -0300

    rename: Renamed verify_timestamp.py to file_validations.py. Also, added a function to verify if loginTicketRequest.xml exists.

--- service/utils/file_validations.py
+++ service/utils/file_validations.py
@@ -4,22 +4,25 @@ from service.utils.logger import logger
 
 
 def timestamp_exists() -> bool:
-
     time_path = "service/time/actual_hour_epoch.txt"
     
     if os.path.exists(time_path):
-        logger.debug("Timestamp exists")
         return True
     else:
-        logger.debug("Timestamp not exists")
         return  False
+    
+def login_ticket_request_exists() -> bool:
+    xml_path = "service/xml_management/loginTicketRequest.xml"
 
+    if os.path.exists(xml_path):
+        return True
+    else:
+        return False
+    
 def login_ticket_response_exists() -> bool:
     xml_path = "service/xml_management/loginTicketResponse.xml"
 
     if os.path.exists(xml_path):
-        logger.debug("loginTicketResponse exists")
         return True
     else:
-        logger.debug("loginTicketResponse not exists")
         return False
