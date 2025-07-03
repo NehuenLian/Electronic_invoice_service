65360664b05f43a3d64d112a2f7981103b607ed8
Nehuen Lian Bova <nehuenn.lian@gmail.com>
2025-07-03 19:39:39 -0300

    new file: "analize_response.py". this file contains functions that verifies and finds specific errors in the responses.

---
+++ service/soap_handler/analize_response.py
@@ -0,0 +1,35 @@
+import json
+from service.utils.logger import logger
+
+
+def response_has_errors() -> bool:
+    
+    with open("CAE_response.json", "r", encoding="utf-8") as file:
+        response_json = file.read()
+    
+    response_json_dict = json.loads(response_json)
+    
+    if response_json_dict['Errors']:
+        logger.info("Errors identified in the response.")
+        return True
+    
+    else:
+        logger.info("No errors in the response.")
+        return False
+
+def find_error():
+
+    with open("CAE_response.json", "r", encoding="utf-8") as file:
+        response_json = file.read()
+
+    response_json_dict = json.loads(response_json)
+
+    errors_level = response_json_dict['Errors']
+
+    err_level = errors_level['Err']
+
+    error_code = err_level[0]['Code']
+    error_message = err_level[0]['Msg']
+
+    print(f'Error code: {error_code}')
+    print(f'Error message: {error_message}')
