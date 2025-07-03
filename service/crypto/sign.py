b703f32472b8b3d84996361b4896a31dd809b05c
Nehuen Lian Bova <nehuenn.lian@gmail.com>
2025-07-03 01:48:09 -0300

    update paths. xml_files/ folder added

--- service/crypto/sign.py
+++ service/crypto/sign.py
@@ -10,8 +10,8 @@ def sign_login_ticket_request():
     openssl_path = "C:\\Program Files\\OpenSSL-Win64\\bin\\openssl.exe"
     sign_command = [
         openssl_path, "cms", "-sign",
-        "-in", "service/xml_management/LoginTicketRequest.xml",
-        "-out", "service/crypto/LoginTicketRequest.xml.cms",
+        "-in", "service/xml_management/xml_files/loginTicketRequest.xml",
+        "-out", "service/crypto/loginTicketRequest.xml.cms",
         "-signer", "service/certificates/returned_certificate.pem",
         "-inkey", "service/certificates/PrivateKey.key",
         "-nodetach",
@@ -22,8 +22,8 @@ def sign_login_ticket_request():
     """
     sign_command = [ 
         "openssl", "cms", "-sign",
-        "-in", "service/xml_files/LoginTicketRequest.xml",
-        "-out", "./LoginTicketRequest.xml.cms",
+        "-in", "service/xml_files/xml_files/loginTicketRequest.xml",
+        "-out", "./loginTicketRequest.xml.cms",
         "-signer", "service/certificates/returned_certificate.pem",
         "-inkey", "service/certificates/PrivateKey.key",
         "-nodetach",
@@ -32,7 +32,12 @@ def sign_login_ticket_request():
     """
     
     result_cms = subprocess.run(sign_command, capture_output=True, text=True)
-    logger.debug("loginTicketRequest.xml successfully signed.")
+    
+    if result_cms.returncode != 0:
+        logger.error(f"Error signing CMS: {result_cms.stderr}")
+        raise Exception("CMS signing failed.")
+    else:
+        logger.debug("loginTicketRequest.xml successfully signed.")
 
 def get_binary_cms() -> str:
     with open("service/crypto/LoginTicketRequest.xml.cms", 'rb') as cms:
