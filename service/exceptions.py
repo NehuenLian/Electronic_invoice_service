72c89c7012f599521954cd4aee241c7f46dff81b
Nehuen Lian Bova <nehuenn.lian@gmail.com>
2025-07-03 01:54:42 -0300

    remove custom exceptions. not necessary anymore

--- service/exceptions.py
+++
@@ -1,7 +0,0 @@
-class AlreadyValidAT(Exception):
-    def __init__(self, original_exception):
-        super().__init__(str(original_exception))
-        self.original_exception = original_exception
-
-class loginTicketResponseNotFound(Exception):
-    pass
