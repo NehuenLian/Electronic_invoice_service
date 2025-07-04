bfee8ad882370bf756d91bf9c7e52f78e934a98a
Nehuen Lian Bova <nehuenn.lian@gmail.com>
2025-07-03 22:37:16 -0300

    update logs, update comments. remove response_has_errors() verification from login_cms().

--- service/soap_handler/soap_client.py
+++ service/soap_handler/soap_client.py
@@ -26,16 +26,13 @@ def login_cms(b64_cms: str) -> str:
         client = Client(wsdl=afip_wsdl)
         login_ticket_response = client.service.loginCms(b64_cms)
         logger.info("CMS login request to AFIP ended successfully.")
-
-        if response_has_errors():
-            find_error()
-            
+        
+        logger.debug(f"login_ticket_response: {login_ticket_response}")
         return login_ticket_response
 
     except Fault as e:
         logger.debug(f"SOAP FAULT in login_cms: {e}")
         # TODO: This needs to be handled in a sophisticated way.  
-        # Verify the presence of '"Errors": {"Err": [{...}]}' in the response.
 
     except Exception as e:
         logger.error(f"General exception in login_cms: {e}")
@@ -51,7 +48,7 @@ def login_cms(b64_cms: str) -> str:
     )
 def fecae_solicitar(full_built_invoice: dict) -> dict:
 
-    logger.debug(f"full_built_invoice in fecae_solicitar: {full_built_invoice}")
+    logger.info(f"Generating invoice...")
     afip_wsdl = "https://wswhomo.afip.gov.ar/wsfev1/service.asmx?WSDL"
 
     try:
@@ -60,13 +57,13 @@ def fecae_solicitar(full_built_invoice: dict) -> dict:
 
         if response_has_errors():
             find_error()
-
+        
+        logger.debug(f"response: {response_cae}")
         return response_cae
 
     except Fault as e:
         logger.debug(f"SOAP FAULT in fecae_solicitar: {e}")
         # TODO: This needs to be handled in a sophisticated way.  
-        # Verify the presence of '"Errors": {"Err": [{...}]}' in the response.
 
     except Exception as e:
         logger.error(f"General exception in fecae_solicitar: {e}")
