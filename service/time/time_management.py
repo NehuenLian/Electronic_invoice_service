808c4780b05e215e87172641f313ab6bab50c457
Nehuen Lian Bova <nehuenn.lian@gmail.com>
2025-07-03 22:25:00 -0300

    update .gitignore: remove ignore actual_hour_epoch.txt file. update time_management.py: remove save actual_hour_epoch.txt file

--- service/time/time_management.py
+++ service/time/time_management.py
@@ -14,10 +14,4 @@ def generate_timestamp() -> tuple[str, str, str]:
     generation_time = utc_now.strftime("%Y-%m-%dT%H:%M:%SZ")
     expiration_time = (utc_now + timedelta(minutes=10)).strftime("%Y-%m-%dT%H:%M:%SZ")
 
-    path = "service/time/actual_hour_epoch.txt"
-    os.makedirs(os.path.dirname(path), exist_ok=True)
-
-    with open(path, 'w', encoding='utf-8') as file:
-        file.write(str(actual_hour))
-
     return actual_hour, generation_time, expiration_time
