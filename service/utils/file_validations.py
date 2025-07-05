93b8238342d3a551b12d64c6fdc0befe2417dd76
Nehuen Lian Bova <nehuenn.lian@gmail.com>
2025-07-04 23:06:08 -0300

    "loginTicketRequest_exists" and "loginTicketResponse_exists" now unified in one "xml_exists" general file. verify_timestamp() function removed. not necessary anymore.

--- service/utils/file_validations.py
+++ service/utils/file_validations.py
@@ -3,24 +3,8 @@ import os
 from service.utils.logger import logger
 
 
-def timestamp_exists() -> bool:
-    time_path = "service/time/actual_hour_epoch.txt"
-    
-    if os.path.exists(time_path):
-        return True
-    else:
-        return  False
-    
-def login_ticket_request_exists() -> bool:
-    xml_path = "service/xml_management/xml_files/loginTicketRequest.xml"
-
-    if os.path.exists(xml_path):
-        return True
-    else:
-        return False
-    
-def login_ticket_response_exists() -> bool:
-    xml_path = "service/xml_management/xml_files/loginTicketResponse.xml"
+def xml_exists(xml_name: str) -> bool:
+    xml_path = f"service/xml_management/xml_files/{xml_name}"
 
     if os.path.exists(xml_path):
         return True
